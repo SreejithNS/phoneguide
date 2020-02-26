24909bd1830da53f04a5d9a36ba8d8bb3b05711d
SreejithNS <nssreejith229@gmail.com>
2020-02-26 15:10:39 +0530

    Added price based query refiner
    grade function is now shifted to global scope

--- functions/index.js
+++ functions/index.js
@@ -74,6 +74,22 @@ module.exports = {
   adminAPI
 }
 
+const grade = (num) => {
+  if (Array.isArray(num)) return false;
+  switch (num) {
+    case 4:
+      return "A"
+    case 3:
+      return "B";
+    case 2:
+      return "C";
+    case 1:
+      return "D";
+    default:
+      return num;
+  }
+}
+
 function queryBuilder(ref, array) {
   const parameter = (str) => {
     switch (str) {
@@ -105,35 +121,65 @@ function queryBuilder(ref, array) {
         return "price";
     }
   }
-  const grade = (num) => {
-    if (Array.isArray(num)) return false;
-    switch (num) {
-      case 4:
-        return "A"
-      case 3:
-        return "B";
-      case 2:
-        return "C";
-      case 1:
-        return "D";
-      default:
-        return num;
-    }
-  }
   const query = (ref, arr, i, p) => {
     if (i < arr.length) {
       let param = parameter(arr[i][0])
       let value = p || grade(arr[i][1])
       if (value) {
-        // if(fromLetters(value)<4 && value !=="YES" && value !== "NO"){
-        //   return query(ref.where(param, "==", value), arr, i,toLetters(fromLetters(value)+1))
-        // }else{
         return query(ref.where(param, "==", value), arr, ++i)
-        //}
       }
     } else {
       return ref
     }
   }
   return query(ref, array, 0)
+}
+
+function priceBasedQueryRefiner(params){
+  const price = params.find(parameter=>parameter[0]==="pr")[1];
+  const limitParamater = (param,limits)=>{
+    limits = new Map(limits);
+    if(limits.has(param) && param[1]>limits.get(param)){
+      param[1] = limits.get(param)
+    }
+    return param;
+  }
+  switch(price){
+    case "LOWBUDGET":
+      params.forEach((param,index)=>{
+        params[index] = limitParamater(param,[
+          ["st",4],
+          ["pe",3],
+          ["ba",4],
+          ["fr",3],
+          ["re",4],
+        ])
+      })
+    break;
+    case "BUDGET":
+      params.forEach((param,index)=>{
+        params[index] = limitParamater(param,[
+          ["ra",3],
+          ["st",4],
+          ["pe",4],
+          ["ba",4],
+          ["fr",4],
+          ["re",4],
+        ])
+      })
+    break;
+    case "FLAGSHIP":
+      params.forEach((param,index)=>{
+        params[index] = limitParamater(param,[
+          ["ra",4],
+          ["st",4],
+          ["pe",4],
+          ["ba",3],
+          ["fr",2],
+          ["re",2],
+        ])
+      })
+    break;
+  }
+  return params
 }
